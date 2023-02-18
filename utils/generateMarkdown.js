39daa8bdb9547e38bb34a57fb7053c5f5f7f8dcd
LisaMLorenz <hej@frau-lorenz.de>
2023-02-18 14:43:42 +0000

    spelling mistake in LICENSE

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,35 +7,35 @@ function generateMarkdown(data, licenseBadge, licenseNotice) {
   switch (data.license) {
     case 'MIT':
       licenseBadge = "[![L[LICENSE] MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      licenseNotice = "This project is licensed under the MIT license - full [LICENSE](./licenses/LICENCE-MIT.txt)";
+      licenseNotice = "This project is licensed under the MIT license - full [LICENSE](./licenses/LICENSE-MIT.txt)";
       break;
     case 'Apache 2.0':
-      licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-      licenseNotice = "This project is licensed under the Apache License 2.0. - full [LICENSE](./licenses/LICENCE-APACHE-V2.txt)";
+      licenseBadge = "[![LICENSE](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      licenseNotice = "This project is licensed under the Apache License 2.0. - full [LICENSE](./licenses/LICENSE-APACHE-V2.txt)";
       break;
     case 'GNU v3.0':
       licenseBadge = "[![L[LICENSE] GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-      licenseNotice = "This project is licensed under the GNU General Public License v3.0. - full [LICENSE](./licenses/LICENCE-GNU-GP-V3.txt)";
+      licenseNotice = "This project is licensed under the GNU General Public License v3.0. - full [LICENSE](./licenses/LICENSE-GNU-GP-V3.txt)";
       break;
     case 'BSD 3-Clause':
       licenseBadge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-      licenseNotice = "This project is licensed under the BSD 3-Clause license - full [LICENSE](./licenses/LICENCE-BSD3CL.txt)";
+      licenseNotice = "This project is licensed under the BSD 3-Clause license - full [LICENSE](./licenses/LICENSE-BSD3CL.txt)";
       break;
     case 'Boost':
       licenseBadge = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-      licenseNotice = "This project is licensed under the Boost Software License 1.0. - full [LICENSE](./licenses/LICENCE-BOOST.txt)";
+      licenseNotice = "This project is licensed under the Boost Software License 1.0. - full [LICENSE](./licenses/LICENSE-BOOST.txt)";
       break;
     case 'Eclipse':
       licenseBadge = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-      licenseNotice = "This project is licensed under the Eclipse Public License 1.0. - full [LICENSE](./licenses/LICENCE-ECLIPSE.txt)";
+      licenseNotice = "This project is licensed under the Eclipse Public License 1.0. - full [LICENSE](./licenses/LICENSE-ECLIPSE.txt)";
       break;
     case 'Mozilla Public':
       licenseBadge = "[![L[LICENSE] MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-      licenseNotice = "This project is licensed under the Mozilla Public License 2.0. - full [LICENSE](./licenses/LICENCE-MOZILLA.txt)";
+      licenseNotice = "This project is licensed under the Mozilla Public License 2.0. - full [LICENSE](./licenses/LICENSE-MOZILLA.txt)";
       break;
     case 'The Unlicense':
       licenseBadge = "[![L[LICENSE] Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
-      licenseNotice = "This project is licensed under the Unlicense - full [LICENSE](./licenses/LICENCE-UNLICENSE.txt)";
+      licenseNotice = "This project is licensed under the Unlicense - full [LICENSE](./licenses/LICENSE-UNLICENSE.txt)";
       break;
     default:
       // No license selected
